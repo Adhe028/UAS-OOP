3765e203ef1c212ef435354f727605c9722cb860
Adhe kurniawan <46317980+Adhe028@users.noreply.github.com>
2020-07-13 16:41:00 +0700

    alhamdulilla selesai

---
+++ UAS OOP/18090088_AdeKurniawan/src/test/java/UAS_OOP/_AdeKurniawan/ApplicationTests.java
@@ -0,0 +1,13 @@
+package UAS_OOP._AdeKurniawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
